c2db4b87dc5af03553c3eec3458ced6693c5588a
FlominatorGD <github.flominatorgd@gmail.com>
2021-11-06 17:25:01 +0100

    adb: on boot
    active on boot in eng builds and no errors anymore

--- system_prop.mk
+++ system_prop.mk
@@ -32,11 +32,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0 \
     wifi.direct.interface=p2p-dev-wlan0 \
     net.tethering.noprovisioning=true
-
-# USB
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.usb.config=mtp
-
+    
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.stagefright.ccodec=0

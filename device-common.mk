e773917e4553001e09ff207285b07ff3a7d21b9c
flominatorgd <github.flominatorgd@gmail.com>
2021-11-27 00:09:53 +0100

    universal7580: lineage-fe

--- device-common.mk
+++ device-common.mk
@@ -18,7 +18,7 @@ LOCAL_PATH := device/samsung/universal7580-common
 
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-havoc
+    $(LOCAL_PATH)/overlay-lineage
 
 # Prebuilt Packages
 PRODUCT_PACKAGES += \
@@ -210,8 +210,6 @@ PRODUCT_PACKAGES += \
     wpa_supplicant.conf \
     android.hardware.wifi@1.0-service.legacy
 
-# Remove WallpapersBReel2020
-$(shell mkdir -p vendor/gapps/product/packages/apps/WallpapersBReel2020 && rm -rf vendor/gapps/product/packages/apps/WallpapersBReel2020 && cp -r device/samsung/universal7580-common/RemovePackages/config.txt vendor/gapps && rm -rf vendor/gapps/config.mk && mv vendor/gapps/config.txt vendor/gapps/config.mk)
 # Properties
 -include $(LOCAL_PATH)/system_prop.mk
 
